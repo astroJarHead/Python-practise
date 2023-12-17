56f5fe4653b95c734916e83fad8ddfe0e63f87d1
astroJarHead <73755825+astroJarHead@users.noreply.github.com>
2023-12-17 16:38:57 -0700

    Add files via upload
    The favorite food challenge Python exercise from the LinkedIn "Programming Foundations: Fundamentals" course

---
+++ food_challenge.py
@@ -0,0 +1,21 @@
+# Challenge 2
+print("Challenge #2")
+
+#Initialize variable
+my_fav = "ice cream"
+
+#Get favorite food of user
+fav_input = input("What's your favorite food? ")
+# Improve output appearance
+print(" ")
+
+# Conditional test
+if my_fav == fav_input:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+    
+# Game is over
+print("**********")
+print("Thanks for playing!")
+print("**********")
